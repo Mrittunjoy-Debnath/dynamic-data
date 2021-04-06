9be5f2551e50d7db58d453649928bdc4e1a4d165
RUHUL6093 <ruhulamin1306@gmail.com>
2021-04-06 21:32:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['Ruhul Amin','Mrittu','Prosen'];
+  const products = [
+  {name:'Photoshop',price : '200$'},
+  {name:'Illustrator',price : '150$'},
+  {name:'Adobe',price : '100$'}
+  ]
+
+
+  const productsName= products.map(product=>product.name);
+  console.log(productsName);
+ const productsPrice= products.map(product=>product.price);
+   console.log(productsPrice);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Counter></Counter>
+
+        <ul>
+            {nayoks.map(nayok=><li>{nayok}</li>)}
+
+            {products.map(product=><li>{product.name}</li>)}
+        </ul>
+
+        {
+          products.map(product =><Product product={product}></Product> )
+        }
+
       </header>
     </div>
   );
 }
 
+function Counter(){
+  const [count,setCount] = useState(10);
+  const handleIncrease =()=>{
+    const newCount = count+1;
+    setCount(newCount);
+  };
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={()=>setCount(count-1)}>Decrease</button>
+      <button onClick={handleIncrease}>Increase</button>
+    </div>
+  )
+}
+
+
+
+function Product(props){
+  const productStyle={
+border : '2px solid yellow',
+borderRadius : '5px solid white',
+backgroundColor: 'skyblue',
+height : '300px',
+width : '300px',
+float : 'left'
+  }
+
+  const {name,price}=props.product;
+  return (
+    <div style={productStyle}>
+      <h1>Name : {props.product.name}</h1>
+      <h3>Play : {props.product.price}</h3>
+      <button>Buy Now</button>
+    </div>
+    )
+}
 export default App;
